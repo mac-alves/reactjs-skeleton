f31325602f421ffd3fc3d2e59426c27908ef9804
mac-alves <mauricio.bleck7@gmail.com>
2020-07-02 16:48:24 -0300

    add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const configJson: AxiosRequestConfig = {
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.API_URL,
     headers: {
         "Content-type": "application/json"
     }
